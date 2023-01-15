be7c62a66456f66fc5ffbf5a1cffb133c94a5380
Suphiozturk8 <suphiozturk8@gmail.com>
2023-01-16 01:06:03 +0300

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -4,5 +4,5 @@ import os
 
 class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
+    API_HASH = os.environ.get("API_HASH", "dcadf01f9a76befff2eccc932c6eabd1")
+    TOKEN = os.environ.get("TOKEN", "5978809708:AAGK_ZTESZHLXup3t3v8uR37f8lvi-HTl68")
