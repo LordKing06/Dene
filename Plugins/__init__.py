f622a61ab0c404d20046f0d70c26d30c0b296140
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-04-24 01:18:55 +0300

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -11,5 +11,5 @@ import time
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-bot = TelegramClient('Maho', api_id=Config.APP_ID, api_hash=Config.API_HASH)
-Maho = bot.start(bot_token=Config.TOKEN)
+bot = TelegramClient('Maho', api_id=Config.API_ID, api_hash=Config.API_HASH)
+Maho = bot.start(bot_token=Config.BOT_TOKEN)
