7f78b01fad799b822dedd8f2254bdec98d6133db
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-04-30 02:38:40 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -16,8 +16,11 @@ LANGAUGE = "TR"
 # test projesi..
 BOT_USERNAME = "TestTagger_bot"
 GROUP_SUPPORT = "TaliaSupport"
-sahib = 'Mahoaga'
+sahib = 'Mahoaga' 
+kanal = 'TaliaSupport'
 startmesaj = "**\n\n➣ Grubundaki tüm üyeleri etiketleyebilirim...\n\n➣ Komutlar butonuna tıklayarak tüm komutları öğrenebilirsin.**" 
 qrupstart = "➥ Şuan aktif bir şekide çalışmaktayım...\n\n➥ Komutları görmek için aşağıdaki komutlar butonuna tıklayın. "
 noadmin = "**🚫 Sen admin değilsin\n\n Yöneticilerle görüşebilirsin.**"
 nogroup = "**Üzgünüm, bu komutu sadece gruplarda kullanabilirsin.**"
+nomesaj = "**Etiket işlemine başlamam için bana bir metin ver**"
+
