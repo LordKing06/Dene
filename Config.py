c82692515d14919a07de559ac0779547ebcf440b
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-04-30 02:33:53 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,16 +1,21 @@
 # Zorunlu gereklidir. Eklemek istediğin bilgileri burda belirt çekinme 😏
-
 import os
-
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "28496124"))
     API_HASH = os.environ.get("API_HASH", "dcadf01f9a76befff2eccc932c6eabd1")
     BOT_TOKEN = os.environ.get("BOT_TOKEN",)
     
     
+############## DEĞİŞKENLER ##############
+DATABASE_URL = "mongodb+srv://lok:lok31@cluster0.lgjlurr.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL = -1001930738807
+GONDERME_TURU = False
+OWNER_ID = [1957316197]
+LANGAUGE = "TR"
+
 # test projesi..
-BOT_USERNAME = 'TestTagger_bot' 
-GROUP_SUPPORT = 'TaliaSupport'
+BOT_USERNAME = "TestTagger_bot"
+GROUP_SUPPORT = "TaliaSupport"
 sahib = 'Mahoaga'
 startmesaj = "**\n\n➣ Grubundaki tüm üyeleri etiketleyebilirim...\n\n➣ Komutlar butonuna tıklayarak tüm komutları öğrenebilirsin.**" 
 qrupstart = "➥ Şuan aktif bir şekide çalışmaktayım...\n\n➥ Komutları görmek için aşağıdaki komutlar butonuna tıklayın. "
