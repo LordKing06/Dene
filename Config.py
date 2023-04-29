3f56c93251fc55d7331cc2af7f41b19e37d7c174
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-04-30 02:13:33 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,3 +6,9 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", "28496124"))
     API_HASH = os.environ.get("API_HASH", "dcadf01f9a76befff2eccc932c6eabd1")
     BOT_TOKEN = os.environ.get("BOT_TOKEN",)
+    
+    
+# test projesi..
+BOT_USERNAME = 'TestTagger_bot' 
+GROUP_SUPPORT = "TaliaSupport"
+sahib = 'Mahoaga 
