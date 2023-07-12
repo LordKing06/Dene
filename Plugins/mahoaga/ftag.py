9cb0bb5450599e581378587bce88ff3fc2ba94b3
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-13 01:18:36 +0300

    Update ftag.py

--- Plugins/mahoaga/ftag.py
+++ Plugins/mahoaga/ftag.py
@@ -1,104 +1,133 @@
-import os, logging, asyncio
-from Plugins import Maho
-from telethon import events, Button
+# Etiketleme işlemi 🤫
+
+import os
+import logging
+import asyncio
+from telethon import Button, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
-from Config import *
-from asyncio import sleep 
-import time, random
+from telethon.tl.types import PeerChannel, ChannelParticipantsRecent, ChannelParticipantsBots 
 
-# Bunlar babadan yadigar :) 
+
+from asyncio import sleep
+from Plugins.mode.config import Maho
+import time
+import random
+
 anlik_calisan = []
 rxyzdev_tagTot = {}
 rxyzdev_initT = {}
 
-# Sonlandırır kapatır. 
+# Komutlar
 @Maho.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
-  if not event.chat_id in anlik_calisan:
-    return
-  else:
-    try:
-      anlik_calisan.remove(event.chat_id)
-    except:
-      pass
-    return await event.respond('**✅ Etiket işlemi başarıyla durduruldu.**')
+    if event.chat_id not in anlik_calisan:
+        return
+    else:
+        try:
+            anlik_calisan.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond('**✅ Etiket işlemi başarıyla durduruldu.**')
 
-
-@Maho.on(events.NewMessage(pattern="^/futbol ?(.*)"))
+@Maho.on(events.NewMessage(pattern="^/ftag ?(.*)"))
 async def mentionall(event):
-  global anlik_calisan 
-  rxyzdev_tagTot[event.chat_id] = 0
-  if event.is_private:
-    return await event.respond("**Bu komutu sadece grup veya kanallarda kullanabilirsiniz.**")
+    global anlik_calisan 
+    rxyzdev_tagTot[event.chat_id] = 0
+    if event.is_private:
+        return await event.respond("**Bu komutu sadece grup veya kanallarda kullanabilirsiniz.**")
   
-  admins = []
-  async for admin in Maho.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
-    return await event.respond("**Bu komutu sadece yöneticiler kullanabilir.**")
+    admins = []
+    async for admin in Maho.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+        admins.append(admin.id)
+    if event.sender_id not in admins:
+        return await event.respond("**🚫 Sen admin değilsin\n\n Yöneticilerle görüşebilirsin.**")
   
-  if event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
-    mode = "text_on_reply"
-    msg = event.reply_to_msg_id
-    if msg == None:
-        return await event.respond("**Eski Mesajlar için Üyelerden Bahsedemem! (gruba eklemeden önce gönderilen mesajlar)**")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("**Bana bir metin verin.**")
-  else:
-    return await event.respond("**Etikete Başlamak için sebep yazın... ✋\n\n(Örnek:** `/futbol Herkese Merhaba!`**)**")
+    if event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.reply_to_msg_id:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond("**Eski Mesajlar için Üyelerden Bahsedemem! (gruba eklemeden önce gönderilen mesajlar)**")
+    else:
+        return await event.respond("**Etikete başlamak için sebep yazın.\n\n(Örnek:** `/etag  Merhaba!`**)**")
   
-  if mode == "text_on_cmd":
-    anlik_calisan.append(event.chat_id)
-    usrnum = 0
-    usrtxt = ""
-    await event.respond("**✅ Etiket işlemi başladı.**")
-        
-    async for usr in Maho.iter_participants(event.chat_id, aggressive=False):
-      rxyzdev_tagTot[event.chat_id] += 1
-      usrnum += 1
-      usrtxt += f"⌯ [{random.choice(futbol)}](tg://user?id={usr.id})\n"
-      if event.chat_id not in anlik_calisan:
-        return
-      if usrnum == 5:
-        await Maho.send_message(event.chat_id, f"**⌯ 📢 {msg}**\n\n{usrtxt}")
-        await asyncio.sleep(2.9)
+    group_participants = await Maho.get_participants(event.chat_id)
+
+    if mode == "text_on_cmd":
+        anlik_calisan.append(event.chat_id)
         usrnum = 0
         usrtxt = ""
-        
-    sender = await event.get_sender()
-    rxyzdev_initT = f"{sender.first_name}"      
-    if event.chat_id in rxyzdev_tagTot:
-           a = await event.respond(f"**✅ Etiket işlemi başarıyla durduruldu.**\n\n**Etiketlenen Kişi Sayısı:** {rxyzdev_tagTot[event.chat_id]}")
+        rxyzdev_tagTot[event.chat_id] = len(group_participants)
+        await event.respond("✅ Etiket işlemi başladı.")
+
+        for usr in group_participants:
+            if usr.deleted or usr.bot:
+                continue 
+
+            usrnum += 1
+            usrtxt += f"⌯ [{random.choice(futbol)}](tg://user?id={x.id})\n"
+
+            if event.chat_id not in anlik_calisan:
+                return
+
+            if usrnum == 5:
+                await Maho.send_message(event.chat_id, f"⌯ 📢 {msg}\n\n{usrtxt}")
+                await asyncio.sleep(8)
+                usrnum = 0
+                usrtxt = ""
+
+        sender = await event.get_sender()
+        rxyzdev_initT = f"[{sender.first_name}](tg://user?id={sender.id})"
+
+        if event.chat_id in rxyzdev_tagTot:
+           member_count = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsRecent())
+           bot_count = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsBots())
+           tag_count = rxyzdev_tagTot[event.chat_id]
+           a = await event.respond(f"✅ Etiket işlemi başarıyla durduruldu.\n\nEtiketlenen kişi sayısı: {tag_count}\nToplam üye sayısı: {len(member_count)}\nToplam bot sayısı: {len(bot_count)}")
            await sleep(10)
            await a.delete()
 
-  if mode == "text_on_reply":
-    anlik_calisan.append(event.chat_id)
- 
-    usrnum = 0
-    usrtxt = ""
-    async for usr in Maho.iter_participants(event.chat_id, aggressive=False):
-      rxyzdev_tagTot[event.chat_id] += 1
-      usrnum += 1
-      usrtxt += f"⌯ [{random.choice(futbol)}](tg://user?id={usr.id})"
-      if event.chat_id not in anlik_calisan:
-        return
-      if usrnum == 5:
-        await Maho.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2.9)
+    if mode == "text_on_reply":
+        anlik_calisan.append(event.chat_id)
         usrnum = 0
         usrtxt = ""
-     
-    sender = await event.get_sender()
-    rxyzdev_initT = f"{sender.first_name}"      
-    if event.chat_id in rxyzdev_tagTot:
-           a = await event.respond(f"**✅ Etiket işlemi başarıyla durduruldu.**\n\n**Etiketlenen Kişi Sayısı:** {rxyzdev_tagTot[event.chat_id]}")
+        rxyzdev_tagTot[event.chat_id] = len(group_participants)
+
+        for usr in group_participants:
+            usrnum += 1
+            usrtxt += f"⌯ [{random.choice(futbol)}](tg://user?id={x.id})\n"
+
+            if event.chat_id not in anlik_calisan:
+                return
+
+            if usrnum == 5:
+                await Maho.send_message(event.chat_id, usrtxt, reply_to=msg)
+                await asyncio.sleep(8)
+                usrnum = 0
+                usrtxt = ""
+
+        sender = await event.get_sender()
+        rxyzdev_initT = f"[{sender.first_name}](tg://user?id={sender.id})"
+
+       
+        if event.chat_id in rxyzdev_tagTot:
+           member_count = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsRecent())
+           bot_count = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsBots())
+           tag_count = rxyzdev_tagTot[event.chat_id]
+           a = await event.respond(f"✅ Etiket işlemi başarıyla durduruldu.\n\nEtiketlenen kişi sayısı: {tag_count}\nToplam üye sayısı: {len(member_count)}\nToplam bot sayısı: {len(bot_count)}")
            await sleep(10)
            await a.delete()
+
+# Bayrak ile Tag işlemi Aşağıdaki gibidir.
+
+bayrak = "🏳️‍🌈 🏳️‍⚧️ 🇺🇳 🇦🇫 🇦🇽 🇦🇱 🇩🇿 🇦🇸 🇦🇩 🇦🇴 🇦🇮 🇦🇶 🇦🇬 🇦🇷 🇦🇲 🇦🇼 🇦🇺 🇦🇹 🇦🇿 🇧🇸 🇧🇭 🇧🇩  🇧🇧 🇧🇾 🇧🇪 🇧🇿 🇧🇯 🇧🇷 🇧🇼 🇧🇦 🇧🇴 🇧🇹 🇧🇲 🇻🇬 🇧🇳 🇧🇬 🇧🇫 🇧🇮 🇰🇭 🇰🇾 🇧🇶 🇨🇻 🇮🇨 🇨🇦 🇨🇲 🇨🇫 🇹🇩 🇮🇴 🇨🇳 🇨🇱 🇨🇽 🇨🇰 🇨🇩 🇨🇬 🇰🇲 🇨🇴 🇨🇨 🇨🇷 🇨🇿 🇪🇬 🇪🇹 🇪🇺 🇸🇻 🇩🇰 🇨🇮 🇭🇷 🇨🇺 🇨🇼 🇨🇾 🇪🇨 🇩🇴 🇩🇲 🇩🇯 🇬🇶 🇪🇷 🇫🇴 🇫🇰 🇫🇯 🇪🇪 🇸🇿 🇫🇮 🇬🇲 🇬🇦 🇹🇫 🇵🇫 🇬🇫 🇫🇷 🇬🇪 🇩🇪 🇬🇭 🇬🇮 🇬🇷 🇬🇱 🇬🇳 🇬🇬 🇬🇹 🇬🇺 🇬🇵 🇬🇩 🇬🇼 🇬🇾 🇭🇹 🇭🇳 🇭🇰 🇭🇺 🎌 🇮🇪 🇮🇶 🇯🇵 🇯🇲 🇮🇷 🇮🇩 🇮🇹 🇮🇱 🇮🇳 🇮🇸 🇮🇲 🇯🇪 🇯🇴 🇰🇬 🇰🇼 🇱🇷 🇱🇾 🇱🇮 🇱🇦 🇰🇿 🇰🇪 🇱🇻 🇱🇹 🇱🇺 🇱🇧 🇰🇮 🇽🇰 🇱🇸 🇲🇴 🇲🇹 🇲🇱 🇲🇻 🇲🇾 🇲🇼 🇲🇬 🇹🇷 🇹🇱 🇸🇪 🇸🇩 🇸🇧 🇸🇴 🇰🇷".split(" ")
+
+# Emojiler ile Tag işlemi Aşağıdaki gibidir.
+
+emoji = "👻 🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧🦈 🐬 🐋 🐳 🐟 🐠 🐡 🦐 🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍧 🍨".split(" ")
 
 # Aşağıdaki gibidir.
 
