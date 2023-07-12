817ec493080342cc433a9c6e5dd0d902bfc14a93
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-13 02:13:36 +0300

    Update config.py

--- Plugins/mode/config.py
+++ Plugins/mode/config.py
@@ -3,8 +3,8 @@
 from telethon import TelegramClient
 
 # Telegram Client (Telethon)
-API_ID = 
-API_HASH = ""
+API_ID = 28496124
+API_HASH = "dcadf01f9a76befff2eccc932c6eabd1"
 bot_token = "6301074895:AAEDy48YMXe8JmCcjK1A9VKWp_YvU7PThbo"
 
 edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
