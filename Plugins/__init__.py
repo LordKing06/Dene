f343131cd18710fd9e97262d648b8228766fbd41
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-12 16:22:34 +0300

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient 
 import logging
-from Plugins.komekci.edaletconfig import edalet
+from Plugins.mode.config import maho
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
