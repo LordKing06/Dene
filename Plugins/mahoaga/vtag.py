136ee561b8723a6c2c6fa49117f0c729adc828ec
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-13 02:24:59 +0300

    Update vtag.py

--- Plugins/mahoaga/vtag.py
+++ Plugins/mahoaga/vtag.py
@@ -66,7 +66,7 @@ async def mentionall(event):
                 continue 
 
             usrnum += 1
-            usrtxt += f"⌯ [{random.choice(bayrak)}](tg://user?id={x.id})\n"
+            usrtxt += f"⌯ [{random.choice(soru)}](tg://user?id={x.id})\n"
 
             if event.chat_id not in anlik_calisan:
                 return
