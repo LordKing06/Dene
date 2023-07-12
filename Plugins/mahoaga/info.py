8a1cb20296ae15a7718178a0ec0fe675c3f52f2f
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-12 16:50:14 +0300

    Create info.py

---
+++ Plugins/mahoaga/info.py
@@ -0,0 +1,27 @@
+from telethon import events
+import asyncio
+import random
+from telethon import events
+from Plugins.mode.config import Maho
+
+@edalet.on(events.NewMessage(pattern="^/id ?(.*)"))
+async def id(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_id = previous_message.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Kullanıcı Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**kullanıcı  id:** `{user_id}`\n**grup id:** `{chat_id}`")
+
+#Tag_0605 
+    else:
+        user_id = event.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Kullanıcı Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**Kullanıcı id:** `{user_id}`\n**grup id:** `{chat_id}`")
+
+print("Kimlik bilgileri")
