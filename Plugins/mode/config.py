45dcb0c8b268eac0e026473c1ee2802cb16d21c3
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-12 16:20:33 +0300

    Update config.py

--- Plugins/mode/config.py
+++ Plugins/mode/config.py
@@ -1,1 +1,10 @@
-#
+# Telefon config gereksinimleri.
+
+from telethon import TelegramClient
+
+# Telegram Client (Telethon)
+API_ID = 
+API_HASH = ""
+bot_token = ""
+
+edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
