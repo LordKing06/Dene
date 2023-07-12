60baf850f3f99c934d1cb57c07a3b455c14b4fb7
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-12 16:21:57 +0300

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -1,15 +1,6 @@
-from telethon import TelegramClient
+from telethon import TelegramClient 
 import logging
-from Config import *
-import asyncio
-import logging
-import os
-import time 
-
-
+from Plugins.komekci.edaletconfig import edalet
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
-
-bot = TelegramClient('Maho', api_id=Config.API_ID, api_hash=Config.API_HASH)
-Maho = bot.start(bot_token=Config.BOT_TOKEN)
