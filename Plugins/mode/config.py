2867ee027d025fe79e049378ba47ea34c195f6c9
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-13 12:05:14 +0300

    Update config.py

--- Plugins/mode/config.py
+++ Plugins/mode/config.py
@@ -5,7 +5,7 @@ from telethon import TelegramClient
 # Telegram Client (Telethon)
 API_ID = 28496124
 API_HASH = "dcadf01f9a76befff2eccc932c6eabd1"
-bot_token = "6394394743:AAGoZqkgViLwUZWW8xvn6dHsEOecrQBv1mU"
+bot_token = "6074522815:AAEuAk8stbR7igTQmbnCo2rnSZ7-_Wmb6r4"
 
 # test projesi.. 
 noadmin = "**🚫 Sen admin değilsin\n\n Yöneticilerle görüşebilirsin.**"
