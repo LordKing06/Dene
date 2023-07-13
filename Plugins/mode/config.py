cdd65dffea32a9e0ce84657fef616232fa4ba36d
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-13 03:11:00 +0300

    Update config.py

--- Plugins/mode/config.py
+++ Plugins/mode/config.py
@@ -5,6 +5,6 @@ from telethon import TelegramClient
 # Telegram Client (Telethon)
 API_ID = 28496124
 API_HASH = "dcadf01f9a76befff2eccc932c6eabd1"
-bot_token = "6301074895:AAEDy48YMXe8JmCcjK1A9VKWp_YvU7PThbo"
+bot_token = "6394394743:AAGoZqkgViLwUZWW8xvn6dHsEOecrQBv1mU"
 
 Maho = TelegramClient('Maho', API_ID, API_HASH).start(bot_token=bot_token)
