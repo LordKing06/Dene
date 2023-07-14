e13219e772258d05c1f10ef4a0eed2d9f65a6040
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-14 22:32:27 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "28496124"))
     API_HASH = os.environ.get("API_HASH", "dcadf01f9a76befff2eccc932c6eabd1")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6385017772:AAF9HPdca4p2vK532fB94_1oDaLb1PY0oE4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6379409508:AAG79c2g2T8wP8Tp2zL8QGN7azMMVbi6Pc8")
 
 
 # test projesi..
