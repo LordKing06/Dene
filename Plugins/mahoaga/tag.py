81b8862c9cdf6a7880ea502eaa0343254ab300e0
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-15 02:33:11 +0300

    Update tag.py

--- Plugins/mahoaga/tag.py
+++ Plugins/mahoaga/tag.py
@@ -2,11 +2,11 @@ import asyncio
 from telethon import events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
-from telethon.tl.types import PeerChannel, ChannelParticipantsRecent, ChannelParticipantsBots 
+from telethon.tl.types import PeerChannel, ChannelParticipantsRecent, ChannelParticipantsBots
 from asyncio import sleep
 import time
+from telethon.sync import TelegramClient
 from Plugins.mode.config import Maho 
-
 
 anlik_calisan = {}
 etiketlenen_uyeler = {}
@@ -28,7 +28,7 @@ async def mentionall(event):
     elif event.reply_to_msg_id:
         mode = "text_on_reply"
         msg = event.reply_to_msg_id
-        if msg == None:
+        if msg is None:
             return await event.respond("Önceki mesajları etiket işlemi için kullanamıyorum.")
     else:
         return await event.respond("İşleme başlamak için ifade yazınız. 💡")
@@ -50,6 +50,12 @@ async def mentionall(event):
                 await asyncio.sleep(10)
                 usrnum = 0
                 usrtxt = ""
+        genel_uye_sayisi = 0
+        async for _ in Maho.iter_participants(event.chat_id):
+            genel_uye_sayisi += 1
+        await event.respond(f"Etiketlenen Gerçek Üye Sayısı: {etiketlenen_uyeler[event.chat_id]}\nGrubun Şuanki Genel Üye Sayısı: {genel_uye_sayisi}\n\nSilinmiş hesaplar ve botlara etiket atılmamıştır.")
+        del anlik_calisan[event.chat_id]
+        del etiketlenen_uyeler[event.chat_id]
     if mode == "text_on_reply":
         anlik_calisan[event.chat_id] = True
         etiketlenen_uyeler[event.chat_id] = 0
@@ -58,7 +64,7 @@ async def mentionall(event):
         async for usr in Maho.iter_participants(event.chat_id):
             if usr.bot or usr.deleted:
                 continue
-            usrnum += 1            
+            usrnum += 1
             usrtxt += f"📣  [{usr.first_name}](tg://user?id={usr.id}) ,"
             if event.chat_id not in anlik_calisan:
                 return
@@ -68,29 +74,8 @@ async def mentionall(event):
                 await asyncio.sleep(10)
                 usrnum = 0
                 usrtxt = ""
-@Maho.on(events.NewMessage(pattern='^(?i)/cancel'))
-async def cancel(event):
-    global anlik_calisan, etiketlenen_uyeler
-    chat_id = event.chat_id
-    if chat_id in anlik_calisan:
         genel_uye_sayisi = 0
-        async for _ in Maho.iter_participants(chat_id):
+        async for _ in Maho.iter_participants(event.chat_id):
             genel_uye_sayisi += 1
-        await event.respond(f"Etiketlenen Gerçek Üye Sayısı: {etiketlenen_uyeler[chat_id]}\nGrubun Şuanki Genel Üye Sayısı: {genel_uye_sayisi}\n\nSilinmiş hesaplar ve botlara etiket atılmamıştır.")
-        del anlik_calisan[chat_id]
-        del etiketlenen_uyeler[chat_id]
-@Maho.on(events.NewMessage(pattern='^/stats'))
-async def stats(event):
-    global etiketlenen_uyeler
-    chat_id = event.chat_id
-    if chat_id in etiketlenen_uyeler:
-        genel_uye_sayisi = 0
-        async for _ in Maho.iter_participants(chat_id):
-            genel_uye_sayisi += 1
-        etiketlenen_gercek_uye_sayisi = etiketlenen_uyeler[chat_id]
-        await event.respond(f"Genel Üye Sayısı: {genel_uye_sayisi}\nEtiketlenen Gerçek Üye Sayısı: {etiketlenen_gercek_uye_sayisi}")
-@Maho.on(events.NewMessage(pattern='^/clearoutput'))
-async def clear_output(event):
-    await asyncio.sleep(15)
-    await event.delete()
+        await event.respond(f"Etiketlenen Gerçek Üye Sayısı: {etiketlenen_uyeler[event.chat_id]}\nGrubun Şuanki Genel Üye Sayısı: {genel_uye_sayisi}\n\nSilinmiş hesaplar ve botlara etiket atılmamıştır.")
 
