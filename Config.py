1ce9f9660bae010ea3d8c3c8310df4c5485290a2
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-07-15 02:55:29 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "28496124"))
     API_HASH = os.environ.get("API_HASH", "dcadf01f9a76befff2eccc932c6eabd1")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5668815938:AAHjCzSY1GwKnMgGe4YVnxL0ZkOhB8kvjZY")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5946035154:AAHV9lOQKz4VL3iNhwp4ovsEWR9GKM6ux34")
 
 
 # test projesi..
-BOT_NAME = 'TestTagger_bot'
+BOT_NAME = 'Videoizle_bot'
 sahib = 'Mahoaga' 
 kanal = 'TaliaSupport'
 startmesaj = "**\n\n➣ Grubundaki tüm üyeleri etiketleyebilirim...\n\n➣ Komutlar butonuna tıklayarak tüm komutları öğrenebilirsin.**" 
